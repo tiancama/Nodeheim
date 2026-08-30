7c1840916c455a862c9bda1b8830390f87338d84
Christian Camacho <140882681+tiancama@users.noreply.github.com>
2026-08-30 10:32:09 +0200

    feat: add NodeViewModel wrapping Node with INotifyPropertyChanged

--- src/Nodeheim.Editor/NodeViewModel.cs
+++ src/Nodeheim.Editor/NodeViewModel.cs
@@ -1,6 +1,39 @@
-﻿namespace Nodeheim.Editor;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Nodeheim.Domain;
 
-public class NodeViewModel
+namespace Nodeheim.Editor;
+
+public class NodeViewModel : INotifyPropertyChanged
 {
+    private double _x;
+    public double X
+    {
+        get => _x;
+        set
+        {
+            if (_x.Equals(value)) return;
+            _x = value;
+            OnPropertyChanged();
+        }
+    }
+    private double _y;
+
+    public double Y
+    {
+        get => _y;
+        set
+        {
+            if (_y.Equals(value)) return;
+            _y = value;
+            OnPropertyChanged();
+        }
+    }
     
+    private readonly Node _node;
+    public NodeViewModel(Node node) => _node = node;
+    public Guid Id => _node.Id;
+    
+    public event PropertyChangedEventHandler? PropertyChanged;
+    private void OnPropertyChanged([CallerMemberName] string? name = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 }

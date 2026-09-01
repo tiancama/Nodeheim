5978772a0e7a68393b2bf91eff8de7b469fecb13
Christian Camacho <140882681+tiancama@users.noreply.github.com>
2026-09-01 18:41:46 +0200

    feat: add EditorViewModel projecting graph nodes

--- src/Nodeheim.Editor/EditorViewModel.cs
+++ src/Nodeheim.Editor/EditorViewModel.cs
@@ -1,6 +1,20 @@
-﻿namespace Nodeheim.Editor;
+﻿using System.Collections.ObjectModel;
+using Nodeheim.Domain;
+
+namespace Nodeheim.Editor;
 
 public class EditorViewModel
 {
-    
+    private readonly Graph _graph = new();
+
+    public ObservableCollection<NodeViewModel> Nodes { get; } = new();
+
+    public EditorViewModel()
+    {
+        foreach (var node in _graph.Nodes)
+        {
+            NodeViewModel nodeViewModel = new(node);
+            Nodes.Add(nodeViewModel);
+        }
+    }
 }

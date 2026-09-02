b38f433b1ac2acce8b721d21eeafb87051966908
Christian Camacho <140882681+tiancama@users.noreply.github.com>
2026-09-02 20:42:03 +0200

    feat: render node collection on canvas via ItemsControl

--- src/Nodeheim.Editor/EditorViewModel.cs
+++ src/Nodeheim.Editor/EditorViewModel.cs
@@ -9,9 +9,14 @@ public class EditorViewModel
 
     public EditorViewModel()
     {
+        _graph.AddNode(new Node());
+        _graph.AddNode(new Node());
+        _graph.AddNode(new Node());
+        int startX = 666;
         foreach (var node in _graph.Nodes)
         {
-            NodeViewModel nodeViewModel = new(node);
+            NodeViewModel nodeViewModel = new(node) { X = startX, Y = 256 };
+            startX += 42;
             Nodes.Add(nodeViewModel);
         }
     }
--- src/Nodeheim.Editor/MainWindow.axaml.cs
+++ src/Nodeheim.Editor/MainWindow.axaml.cs
@@ -1,24 +1,14 @@
 using Avalonia.Controls;
-using Avalonia.Input;
-using Nodeheim.Domain;
 
 namespace Nodeheim.Editor;
 
 public partial class MainWindow : Window
 {
-    private readonly NodeViewModel _vm;
+    private readonly EditorViewModel _vm = new();
 
     public MainWindow()
     {
         InitializeComponent();
-        var node = new Node();
-        _vm = new NodeViewModel(node) { X = 80, Y = 100 };
         DataContext = _vm;
-    }
-
-    private void OnCoordKeyDown(object? sender, KeyEventArgs e)
-    {
-        if (e.Key == Key.Enter)
-            MainCanvas.Focus();
     }
 }

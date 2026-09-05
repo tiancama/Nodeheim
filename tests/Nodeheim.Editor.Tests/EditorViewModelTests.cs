7286b51c9ea28ea670a0ff9ab85a2d7c07f395f3
Christian Camacho <140882681+tiancama@users.noreply.github.com>
2026-09-05 15:59:36 +0200

    test: add Editor test project pinning BeginDrag idempotency

---
+++ tests/Nodeheim.Editor.Tests/EditorViewModelTests.cs
@@ -0,0 +1,17 @@
+﻿using Nodeheim.Domain;
+
+namespace Nodeheim.Editor.Tests;
+
+public class EditorViewModelTests
+{
+    [Fact]
+    public void BeginDrag_InvokedTwice_DoesNotThrow()
+    {
+        var editor = new EditorViewModel();
+        var point = new SurfacePosition(10, 10);
+        editor.SelectSingleNode(new NodeViewModel(new Node()));
+        editor.BeginDrag(point);
+        Exception? exception = Record.Exception(() => editor.BeginDrag(point));
+        Assert.Null(exception);
+    }
+}

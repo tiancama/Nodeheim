988448fb8ab9cdeb33015515513ab833c29ca50a
Christian Camacho <140882681+tiancama@users.noreply.github.com>
2026-09-25 16:40:45 +0200

    test: pin NodeViewModel change notifications

---
+++ tests/Nodeheim.Editor.Tests/NodeViewModelTests.cs
@@ -0,0 +1,31 @@
+﻿using Nodeheim.Domain;
+
+namespace Nodeheim.Editor.Tests;
+
+public class NodeViewModelTests
+{
+    [Fact]
+    public void X_SetToDifferentValue_RaisesPropertyChanged()
+    {
+        var node = new NodeViewModel(new Node()) { X = 10 };
+        var raised = new List<string?>();
+        node.PropertyChanged += (_, e) => raised.Add(e.PropertyName);
+
+        node.X = 20;
+
+        string? name = Assert.Single(raised);
+        Assert.Equal(nameof(NodeViewModel.X), name);
+    }
+
+    [Fact]
+    public void X_SetToSameValue_DoesNotRaisePropertyChanged()
+    {
+        var node = new NodeViewModel(new Node()) { X = 10 };
+        var raised = new List<string?>();
+        node.PropertyChanged += (_, e) => raised.Add(e.PropertyName);
+
+        node.X = 10;
+
+        Assert.Empty(raised);
+    }
+}
